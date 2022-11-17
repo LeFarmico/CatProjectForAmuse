7bda0f54980fc5e9c3223dcb654cfb07017f04b7
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-17 17:59:17 +0100

    performance: increased performance of fetchCats() function by making requests in parallel (from 10 to 2 sec)

--- app/src/main/java/io/amuse/codeassignment/repository/CatsRepository.kt
+++ app/src/main/java/io/amuse/codeassignment/repository/CatsRepository.kt
@@ -2,14 +2,22 @@ package io.amuse.codeassignment.repository
 
 import io.amuse.codeassignment.domain.api.CatsApi
 import io.amuse.codeassignment.domain.model.Cat
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 
 class CatsRepository {
+
     suspend fun fetchCats(): Flow<List<Cat>> = channelFlow {
         val cats = (0..50).map {
-            CatsApi.getCat()
-        }
-        trySend(cats)
+            async {
+                CatsApi.getCat()
+            }
+        }.toTypedArray()
+
+        // *cats unpacks Array to vararg
+        val response = awaitAll(*cats)
+        trySend(response)
     }
 }
