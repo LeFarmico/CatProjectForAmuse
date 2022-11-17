047b426d8f196618376963a096fcdc37c37571cc
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-17 22:47:19 +0100

    fix: added Inject annotation to CatsApiImpl

--- app/src/main/java/io/amuse/codeassignment/domain/impl/CatsApiImpl.kt
+++ app/src/main/java/io/amuse/codeassignment/domain/impl/CatsApiImpl.kt
@@ -5,12 +5,11 @@ import io.amuse.codeassignment.domain.model.Cat
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import javax.inject.Inject
 
-class CatsApiImpl(
-    private val client: HttpClient = CatsApi.client // TODO change it to DI
-) : CatsApi {
+class CatsApiImpl @Inject constructor(private val client: HttpClient) : CatsApi {
 
     override suspend fun getCat(): Cat {
-        return CatsApi.client.get("https://cataas.com/cat?json=true").body()
+        return client.get("https://cataas.com/cat?json=true").body()
     }
 }
