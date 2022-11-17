2da8455a773a8a9aaa32ead1500d447471b3aea1
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-17 21:56:05 +0100

    feature: added App.kt class and DomainModule.kt which provides HttpClient

---
+++ app/src/main/java/io/amuse/codeassignment/injection/DomainModule.kt
@@ -0,0 +1,31 @@
+package io.amuse.codeassignment.injection
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DomainModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient() = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+    }
+}
