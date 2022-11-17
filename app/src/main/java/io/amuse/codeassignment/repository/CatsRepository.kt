2a9cda6767f65a16cfd5457c10f1d55a14c7e9d1
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-17 22:16:08 +0100

    fix: added Repository module which provide CatsRepository

--- app/src/main/java/io/amuse/codeassignment/repository/CatsRepository.kt
+++ app/src/main/java/io/amuse/codeassignment/repository/CatsRepository.kt
@@ -1,16 +1,18 @@
 package io.amuse.codeassignment.repository
 
-import io.amuse.codeassignment.domain.impl.CatsApiImpl
+import io.amuse.codeassignment.domain.api.CatsApi
 import io.amuse.codeassignment.domain.model.Cat
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
+import javax.inject.Inject
 
-class CatsRepository {
+class CatsRepository @Inject constructor(
+    private val api: CatsApi
+) {
 
     suspend fun fetchCats(): Flow<List<Cat>> = channelFlow {
-        val api = CatsApiImpl()
         val cats = (0..50).map {
             async {
                 api.getCat()
