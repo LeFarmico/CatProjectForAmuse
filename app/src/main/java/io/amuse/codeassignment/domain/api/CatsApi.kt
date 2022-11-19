09ad5ba515cc031fa4f04128386f123998ef2ed5
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-19 19:09:05 +0100

    feature: added network response handler for HttpClient and api

--- app/src/main/java/io/amuse/codeassignment/domain/api/CatsApi.kt
+++ app/src/main/java/io/amuse/codeassignment/domain/api/CatsApi.kt
@@ -1,8 +1,9 @@
 package io.amuse.codeassignment.domain.api
 
 import io.amuse.codeassignment.domain.model.Cat
+import io.amuse.codeassignment.domain.model.NetworkResponse
 
 interface CatsApi {
 
-    suspend fun getCat(): Cat
+    suspend fun getCat(): NetworkResponse<Cat>
 }
--- app/src/main/java/io/amuse/codeassignment/domain/impl/CatsApiImpl.kt
+++ app/src/main/java/io/amuse/codeassignment/domain/impl/CatsApiImpl.kt
@@ -2,14 +2,28 @@ package io.amuse.codeassignment.domain.impl
 
 import io.amuse.codeassignment.domain.api.CatsApi
 import io.amuse.codeassignment.domain.model.Cat
+import io.amuse.codeassignment.domain.model.NetworkResponse
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.plugins.*
 import io.ktor.client.request.*
+import java.nio.channels.UnresolvedAddressException
 import javax.inject.Inject
 
 class CatsApiImpl @Inject constructor(private val client: HttpClient) : CatsApi {
 
-    override suspend fun getCat(): Cat {
-        return client.get("https://cataas.com/cat?json=true").body()
+    override suspend fun getCat(): NetworkResponse<Cat> {
+        return try {
+            val response = client.get("https://cataas.com/dog?json=true")
+            NetworkResponse.Success(response.body())
+        } catch (e: ResponseException) {
+            NetworkResponse.Error(
+                code = e.response.status.value,
+                message = e.message ?: "",
+                throwable = e
+            )
+        } catch (e: UnresolvedAddressException) {
+            NetworkResponse.Exception(e)
+        }
     }
 }
