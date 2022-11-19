5d631d6b8f67c5e366df61d293b3cb1dafc277b4
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-19 21:33:56 +0100

    perf: deleted useless scope in suspend function

--- app/src/main/java/io/amuse/codeassignment/repository/impl/CatsRepositoryImpl.kt
+++ app/src/main/java/io/amuse/codeassignment/repository/impl/CatsRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package io.amuse.codeassignment.repository.impl
 
 import io.amuse.codeassignment.domain.api.CatsApi
-import io.amuse.codeassignment.domain.model.Cat
+import io.amuse.codeassignment.domain.model.CatModel
 import io.amuse.codeassignment.domain.model.NetworkResponse
 import io.amuse.codeassignment.repository.api.CatsRepository
 import kotlinx.coroutines.*
@@ -15,10 +15,9 @@ class CatsRepositoryImpl @Inject constructor(
     override suspend fun fetchCats(
         onStart: () -> Unit,
         onError: (String?) -> Unit
-    ): Flow<List<Cat>> = channelFlow {
-        val scope = CoroutineScope(Dispatchers.IO + Job())
+    ): Flow<List<CatModel>> = channelFlow {
         val cats = (0..50).map {
-            scope.async {
+            async {
                 when (val catResponse = api.getCat()) {
                     is NetworkResponse.Error -> throw (catResponse.throwable) // handle API request errors
                     is NetworkResponse.Exception -> throw (catResponse.throwable) // handle user errors
