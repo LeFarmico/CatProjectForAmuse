33303a2c88b8a1d477179bfad7044208dc49f2c1
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-20 14:13:02 +0100

    feature: added odd and even background color for cat items

--- app/src/main/java/io/amuse/codeassignment/ui/theme/Color.kt
+++ app/src/main/java/io/amuse/codeassignment/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val OddRowColor = Color(0xFF000000)
+val EvenRowColor = Color(0xFF808080)
