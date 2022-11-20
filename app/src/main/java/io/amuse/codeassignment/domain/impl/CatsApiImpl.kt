9c9ccabe9987ba56277bb19e07b594e764a10fd7
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-11-20 14:07:07 +0100

    feature: added mimetype validation by requirements. To follow the requirements added recursion for getting a cat with special mimetype and timeout to stop endless recursion

--- app/src/main/java/io/amuse/codeassignment/domain/impl/CatsApiImpl.kt
+++ app/src/main/java/io/amuse/codeassignment/domain/impl/CatsApiImpl.kt
@@ -1,5 +1,6 @@
 package io.amuse.codeassignment.domain.impl
 
+import io.amuse.codeassignment.domain.CatsURL
 import io.amuse.codeassignment.domain.api.CatsApi
 import io.amuse.codeassignment.domain.model.CatModel
 import io.amuse.codeassignment.domain.model.NetworkResponse
@@ -7,23 +8,41 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.plugins.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.withTimeout
 import java.nio.channels.UnresolvedAddressException
 import javax.inject.Inject
 
 class CatsApiImpl @Inject constructor(private val client: HttpClient) : CatsApi {
 
-    override suspend fun getCat(): NetworkResponse<CatModel> {
+    override suspend fun getJpegCat(): NetworkResponse<CatModel> {
+        val timeout: Long = 5_000
         return try {
-            val response = client.get("https://cataas.com/cat?json=true")
-            NetworkResponse.Success(response.body())
+            // stopping of recursion in case of impossibility to find correct mimetype
+            withTimeout(timeout) {
+                val response = client.get(CatsURL.URL_JSON)
+
+                // filter by mimetype, if it is not image/jpeg then trying to get another cat
+                if ((response.body() as CatModel).mimeType == "image/jpeg") {
+                    NetworkResponse.Success(response.body())
+                } else {
+                    getJpegCat()
+                }
+            }
         } catch (e: ResponseException) {
             NetworkResponse.Error(
                 code = e.response.status.value,
-                message = e.message ?: "",
+                message = e.message,
                 throwable = e
             )
         } catch (e: UnresolvedAddressException) {
             NetworkResponse.Exception(e)
+        } catch (e: CancellationException) {
+            NetworkResponse.Exception(
+                CancellationException(
+                    "Cats with mimetype: image/jpeg haven't found for $timeout millis"
+                )
+            )
         }
     }
 }
